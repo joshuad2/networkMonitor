7bd90f2801be4f9e90458ec6cf995febd0f0c346
Robert Hamel <roberthamel@gmail.com>
2018-12-04 14:59:14 -0500

    feat(client): add back in

---
+++ client/src/main/java/org/networkMonitor/client/domain/Netstat.java
@@ -0,0 +1,22 @@
+package com.example.client.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Netstat {
+    private String address;
+    private String type;
+    private String recvQ;
+    private String sendQ;
+    private String iNode;
+    private String conn;
+    private String refs;
+    private String nextref;
+    private String addr;
+}
