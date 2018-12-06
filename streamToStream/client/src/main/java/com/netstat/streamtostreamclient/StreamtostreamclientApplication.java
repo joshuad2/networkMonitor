2f2dc95c0497e68f731061e80f16dbf9627abf2c
Robert Hamel <roberthamel@gmail.com>
2018-12-06 13:01:05 -0500

    chore(streamtostream): add servers

---
+++ streamToStream/client/src/main/java/com/netstat/streamtostreamclient/StreamtostreamclientApplication.java
@@ -0,0 +1,12 @@
+package com.netstat.streamtostreamclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamtostreamclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamtostreamclientApplication.class, args);
+	}
+}
---
+++ streamToStream/server/src/main/java/com/netstat/streamtostreamserver/StreamtostreamserverApplication.java
@@ -0,0 +1,12 @@
+package com.netstat.streamtostreamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamtostreamserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamtostreamserverApplication.class, args);
+	}
+}
